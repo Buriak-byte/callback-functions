2a3d7e9b4a5a3d277e70dd4d7d4669850eda025a
Buriak-byte <oleksii.buriak12@gmail.com>
2026-04-05 19:45:32 +0300

    Update cb.js

--- cb.js
+++ cb.js
@@ -1,48 +1,8 @@
 //1
-
-// Завдання 1. Лічильник подій 🔢
-
-// Створіть функцію, яка рахує, скільки елементів масиву відповідають певній умові, використовуючи колбек-функцію для перевірки.
-
-
-
-// Створіть функцію countItems(array, condition)
-// Створіть змінну count = 0
-// Пройдіться по масиву циклом for...of
-// Для кожного елемента викличте condition(element)
-// Якщо condition повертає true, збільште count на 1
-// Поверніть кінцевий результат
-
-
-// Початковий код:
-
-// const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
-// const words = ['кіт', 'собака', 'миша', 'папуга', 'хомяк'];
-
-// // Функція countItems тут
-
-// // Умови для перевірки:
-// const isEven = num => num % 2 === 0;
-// const isLarge = num => num > 10;
-// const isShort = word => word.length <= 3;
-
-// // Тестування
-// console.log('Парних чисел:', countItems(numbers, isEven));
-// console.log('Чисел більше 10:', countItems(numbers, isLarge));
-// console.log('Коротких слів:', countItems(words, isShort));
-
-
-
-
-
-
-
-
-
 const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
 const words = ['кіт', 'собака', 'миша', 'папуга', 'хомяк'];
 
-// Функція countItems
+
 function countItems(array, condition) {
   let count = 0;
 
@@ -55,39 +15,18 @@ function countItems(array, condition) {
   return count;
 }
 
-// Умови для перевірки:
+
 const isEven = num => num % 2 === 0;
 const isLarge = num => num > 10;
 const isShort = word => word.length <= 3;
 
-// Тестування
+
 console.log('Парних чисел:', countItems(numbers, isEven));
 console.log('Чисел більше 10:', countItems(numbers, isLarge));
 console.log('Коротких слів:', countItems(words, isShort));
 
 //2
-// Створіть універсальний калькулятор, який може виконувати різні математичні операції залежно від переданої колбек-функції.
 
-
-
-// Створіть стрілкову функцію calculate(a, b, operation)
-// Функція повинна викликати operation(a, b) та повернути результат
-// Створіть 4 стрілкові функції для операцій: add, subtract, multiply, divide
-// У функції divide додайте перевірку на ділення на нуль
-// Протестуйте всі операції
-// Початковий код:
-
-// // Функція calculate тут
-
-// // Стрілкові функції для операцій тут
-
-// // Тестування
-// console.log(calculate(10, 5, add));      // Повинно показати 15
-// console.log(calculate(10, 5, subtract)); // Повинно показати 5
-// console.log(calculate(10, 5, multiply)); // Повинно показати 50
-// console.log(calculate(10, 5, divide));   // Повинно показати 2
-// console.log(calculate(10, 0, divide));   // Повинно показати помилку
-// // 
 const add = (a, b) => a + b
 const substract = (a, b) => a - b
 const multiply = (a, b) => a * b
@@ -106,11 +45,11 @@ return operation(a, b);
 
 
 
-console.log(calculate(10, 5, add));      // Повинно показати 15
- console.log(calculate(10, 5, substract)); // Повинно показати 5
- console.log(calculate(10, 5, multiply)); // Повинно показати 50
- console.log(calculate(10, 5, divide));   // Повинно показати 2
- console.log(calculate(10, 0, divide));   // Повинно показати помилку
+console.log(calculate(10, 5, add));     
+ console.log(calculate(10, 5, substract)); 
+ console.log(calculate(10, 5, multiply));
+ console.log(calculate(10, 5, divide));
+ console.log(calculate(10, 0, divide)); 
 
  //3
 //  Створіть функцію, яка повторює певну дію задану кількість разів, використовуючи колбек-функцію для генерації повідомлень.
@@ -121,11 +60,12 @@ console.log(calculate(10, 5, add));      // Повинно показати 15
 // Створіть цикл for від i = 0 до i < times
 // На кожній ітерації викликайте messageCreator(i)
 // Протестуйте з різними колбеками
-const times = 5;
+let times = 0;
 function repeatMessage(times, messageCreator){
-  for (let i = 0; i < times.length; i++) {
-   return messageCreator[i];
-    
+  for(let i1 = 0; i1 < times.length; i1++){
+ console.log(messageCreator(i));
   }
 }
-console.log(repeatMessage(1, ))
+console.log(repeatMessage(1, ));
+console.log(repeatMessage(1));
+console.log(repeatMessage(1));
